c5110cfa3d55ff69c558c46139a86867c1202b2f
federicopires <59855798+federicopires@users.noreply.github.com>
2020-04-03 16:18:57 -0300

    v0.0.1 (#1)
    * v0.0.1
    Add some Ceibo standards and switch to mixed launch template.
    
    * Some fixes
    
    * Change instance types default
    
    * fix tags and launch template
    
    * Fix tf12 syntax
    
    * Use latest Amazon Linux AMI
    
    * Fix user data encoding
    
    * Add cloudwatch group metrics
    
    * Fix tf12 syntax
    
    * Add back ssh key support
    
    * Volume 0 size
    
    * return sane defaults for asg
    
    * Fix root volume
    
    Co-authored-by: Federico Pires <federico.pires@upsight.com>

--- variables.tf
+++ variables.tf
@@ -1,11 +1,52 @@
 variable "vpc_id" {
+  type        = string
+  description = "VPC ID"
 }
 
-variable "ssh_key_name" {
+variable "region" {
+  type        = string
+  description = "AWS region"
+}
+
+variable "namespace" {
+  type        = string
+  description = "Namespace, which could be your organization name, e.g. 'eg' or 'cp'"
+  default     = ""
+}
+
+variable "stage" {
+  type        = string
+  description = "Stage, e.g. 'prod', 'staging', 'dev', or 'test'"
+  default     = ""
 }
 
 variable "name" {
-  default = "main"
+  type        = string
+  description = "Solution cluster name, e.g. 'main'"
+}
+
+variable "delimiter" {
+  type        = string
+  default     = "-"
+  description = "Delimiter to be used between `name`, `namespace`, `stage`, etc."
+}
+
+variable "attributes" {
+  type        = list(string)
+  default     = []
+  description = "Additional attributes (e.g. `1`)"
+}
+
+variable "tags" {
+  type        = map(string)
+  default     = {}
+  description = "Additional tags (e.g. `map('BusinessUnit`,`XYZ`)"
+}
+
+variable "description" {
+  type        = string
+  default     = ""
+  description = "Elastic Beanstalk Application description"
 }
 
 variable "min_size" {
@@ -23,34 +64,62 @@ variable "max_size" {
   default     = 2
 }
 
-variable "subnet_ids" {
+variable "ec2_subnet_ids" {
   description = "Subnets for RabbitMQ nodes"
   type        = list(string)
 }
 
-variable "nodes_additional_security_group_ids" {
-  type    = list(string)
-  default = []
+variable "elb_subnet_ids" {
+  description = "Subnets for ELB"
+  type        = list(string)
 }
 
-variable "elb_additional_security_group_ids" {
+variable "nodes_security_group_ids" {
   type    = list(string)
-  default = []
+  default = null
+}
+
+variable "elb_security_group_ids" {
+  type    = list(string)
+  default = null
 }
 
 variable "instance_type" {
-  default = "m5.large"
+  default = "t3.small"
 }
 
-variable "instance_volume_type" {
-  default = "standard"
+variable "root_volume_type" {
+  default = "gp2"
 }
 
-variable "instance_volume_size" {
+variable "root_volume_size" {
+  default = "8"
+}
+
+variable "root_volume_iops" {
   default = "0"
 }
 
-variable "instance_volume_iops" {
-  default = "0"
+variable "fallback_ondemand_instance_type" {
+  default     = "t2.small"
+  description = "The fallback instance type if desired is not available"
 }
 
+variable "ondemand_base_capacity" {
+  default     = 2
+  description = "Absolute minimum amount of desired capacity that must be fulfilled by on-demand instances"
+}
+
+variable "ondemand_instance_type" {
+  default     = "t3.small"
+  description = "The desired on-demand instance type"
+}
+
+variable "ondemand_percentage_above_base_capacity" {
+  default     = 100
+  description = "Percentage split between on-demand and Spot instances above the base on-demand capacity"
+}
+
+variable "ssh_key_name" {
+  default = null
+}
